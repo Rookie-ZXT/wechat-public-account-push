a04ef70abc065619c7b6cc7b88203e49069abbdc
Zhouxuanting <90813520+Rookie-ZXT@users.noreply.github.com>
2022-10-07 16:26:32 +0800

    Update index.cjs
    111

--- config/index.cjs
+++ config/index.cjs
@@ -23,14 +23,11 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oNaIX6GMHWg4Ag86gKMrAvntcFiA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'w4fHhitzAZSEKYugwHYQOoj3ZBBo6HFM5sByT3o8Te0',
+      useTemplateId: 'ss1FxiiVzIPTZN75AtqrbNba0F8-sbu8JlxAFV4bOxo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-12',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '2001', date: '01-20',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '宝贝', year: '2001', date: '02-12',
@@ -49,7 +46,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'w4fHhitzAZSEKYugwHYQOoj3ZBBo6HFM5sByT3o8Te0',
+  CALLBACK_TEMPLATE_ID: 'ss1FxiiVzIPTZN75AtqrbNba0F8-sbu8JlxAFV4bOxo',
 
   CALLBACK_USERS: [
     {
