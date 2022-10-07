124a8256cc6bc94475e78f073030057ca23b345f
Zhouxuanting <90813520+Rookie-ZXT@users.noreply.github.com>
2022-10-07 17:02:23 +0800

    Update index.cjs
    111

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '段',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oNaIX6C3LwdOOF-XLNkAZ-9Axphw',
+      id: 'oNaIX6IRdNFueXEVv7u-jJiYFP_s',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'XDQjq0XUPUo0NyQXgvJfzne5MdDTgSSo5WBjfAspkic',
+      useTemplateId: 'w4fHhitzAZSEKYugwHYQOoj3ZBBo6HFM5sByT3o8Te0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-12',
       festivals: [
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'XDQjq0XUPUo0NyQXgvJfzne5MdDTgSSo5WBjfAspkic',
+  CALLBACK_TEMPLATE_ID: 'w4fHhitzAZSEKYugwHYQOoj3ZBBo6HFM5sByT3o8Te0',
 
   CALLBACK_USERS: [
     {
