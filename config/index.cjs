2536a59b74584118e2d7777eb1c5ec3c766350eb
Zhouxuanting <90813520+Rookie-ZXT@users.noreply.github.com>
2022-10-07 17:05:08 +0800

    Update index.cjs
    111

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '段',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oNaIX6IRdNFueXEVv7u-jJiYFP_s',
+      id: 'oNaIX6Hov5cE5qHD_AmJZIKDiE-I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'w4fHhitzAZSEKYugwHYQOoj3ZBBo6HFM5sByT3o8Te0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
